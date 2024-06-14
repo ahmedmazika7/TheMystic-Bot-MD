7a1153b7da244a3e347b5852f1f3bd9d28d015ba
ahmedmazika7 <149413372+ahmedmazika7@users.noreply.github.com>
2024-06-14 08:30:12 +0300

    Update شغل.js

--- plugins/شغل.js
+++ plugins/شغل.js
@@ -65,9 +65,9 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   });
 };
 
-handler.help = ['song'].map((v) => v + ' <query>');
+handler.help = ['هات'].map((v) => v + ' <query>');
 handler.tags = ['downloader'];
-handler.command = /^song$/i;
+handler.command = /^هات$/i;
 
 handler.exp = 0;
 handler.diamond = false;
